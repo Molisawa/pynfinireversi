88eb901451cd7f0b74396cd113b89eef1ad05485
ochavirar <ochavirar@outlook.com>
2023-11-27 07:23:23 -0600

    WIP

--- engine_utils/is_valid_move.py
+++ engine_utils/is_valid_move.py
@@ -14,22 +14,43 @@ def isValidMove(board:Board, lastMove:Movement) -> bool:
     rowIndex = lastMove.y
     offset = 0
 
+    if ( vertical_review(board, rowIndex, colIndex, opponent, lastMove, offset) or 
+        horizontal_review(board, rowIndex, colIndex, opponent, lastMove, offset) or 
+        diagonal_above(board, rowIndex, colIndex, opponent, lastMove, offset) or
+        diagonal_below(board, rowIndex, colIndex, opponent, lastMove, offset) ): 
+            return True
+
+    # when all hopes fade away
+    return False
+
+def vertical_review(board: Board, rowIndex: int, colIndex:int, opponent: int, lastMove: Movement, offset: int) -> bool:
+    '''
+    This is the movement review for the up and down position
+    '''
+    # Move up
     while colIndex > 0 and board.state[colIndex][rowIndex].pieceType == opponent:
         colIndex -= 1
         offset += 1
     if colIndex >= 0 and board.state[colIndex][rowIndex].pieceType == lastMove.pieceType and offset > 0:
         return True
-
+    
     # move down
     colIndex = lastMove.x + 1
     rowIndex = lastMove.y
     offset = 0
+
     while colIndex < board.size - 1 and board.state[colIndex][rowIndex].pieceType == opponent:
         colIndex += 1
         offset += 1
     if colIndex <= board.size - 1 and board.state[colIndex][rowIndex].pieceType == lastMove.pieceType and offset > 0:
         return True
 
+    return False
+
+def horizontal_review(board: Board, rowIndex: int, colIndex:int, opponent: int, lastMove: Movement, offset: int) -> bool:
+    '''
+    This is the movement review for the right and left position
+    '''
     # move left
     colIndex = lastMove.x
     rowIndex = lastMove.y - 1
@@ -39,7 +60,7 @@ def isValidMove(board:Board, lastMove:Movement) -> bool:
         offset += 1
     if rowIndex >= 0 and board.state[colIndex][rowIndex].pieceType == lastMove.pieceType and offset > 0:
         return True
-
+    
     # move right
     colIndex = lastMove.x
     rowIndex = lastMove.y + 1
@@ -50,6 +71,12 @@ def isValidMove(board:Board, lastMove:Movement) -> bool:
     if rowIndex <= board.size - 1 and board.state[colIndex][rowIndex].pieceType == lastMove.pieceType and offset > 0:
         return True
 
+    return False
+
+def diagonal_above(board: Board, rowIndex: int, colIndex:int, opponent: int, lastMove: Movement, offset: int) -> bool:
+    '''
+    This is the movement review for the right and left diagonal position above
+    '''
     # move up left
     colIndex = lastMove.x - 1
     rowIndex = lastMove.y - 1
@@ -72,6 +99,12 @@ def isValidMove(board:Board, lastMove:Movement) -> bool:
     if colIndex >= 0 and rowIndex <= board.size - 1 and board.state[colIndex][rowIndex].pieceType == lastMove.pieceType and offset > 0:
         return True
 
+    return False
+
+def diagonal_below(board: Board, rowIndex: int, colIndex:int, opponent: int, lastMove: Movement, offset: int)->bool:
+    '''
+    This is the movement review for the right and left diagonal position below
+    '''
     # move down left
     colIndex = lastMove.x + 1
     rowIndex = lastMove.y - 1
@@ -94,5 +127,4 @@ def isValidMove(board:Board, lastMove:Movement) -> bool:
     if colIndex <= board.size - 1 and rowIndex <= board.size - 1 and board.state[colIndex][rowIndex].pieceType == lastMove.pieceType and offset > 0:
         return True
 
-    # when all hopes fade away
     return False
