5dc16f51d05db152288ff437c38cfad73130dfdd
ochavirar <ochavirar@outlook.com>
2023-11-27 07:39:59 -0600

    WIP

--- engine_utils/init_board.py
+++ engine_utils/init_board.py
@@ -9,8 +9,20 @@ from builtins import min as fmin
 from engine_classes import *
 
 def initializeBoard(board:Board) -> None:
+    '''
+    Funciton to initialize the board states.
+    Initially, it restards to zero
+    Then, it gives the deffault values for a game with stat
+    '''
     board.lastPiecetypeMoved = 2
 
+    set_states(board)
+    initialize_values(board)
+
+def set_states(board: Board) -> None:
+    '''
+    Reset the states
+    '''
     board.state = [[Piece() for _ in range(board.size)] for _ in range(board.size)]
     for i in range(board.size):
         for j in range(board.size):
@@ -21,6 +33,10 @@ def initializeBoard(board:Board) -> None:
         for j in range(board.size):
             board.initialState[i][j].pieceType = 0
 
+def initialize_values(board: Board):
+    '''
+    Initialize with player values
+    '''
     board.state[board.size // 2 - 1][board.size // 2 - 1].pieceType = 2
     board.state[board.size // 2][board.size // 2].pieceType = 2
     board.state[board.size // 2 - 1][board.size // 2].pieceType = 1
